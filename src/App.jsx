70f1ccb4557adf8483a58113f8c033acd4476e20
alisonvor <alisonvort@gmail.com>
2022-03-16 18:07:29 -0300

    Fim do Curso

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import { useMemo, useState } from "react"
+import { Form } from "./components/Forms"
+
 function App() {
-  return <h1>Olar Mundo!</h1>
+  const [ tasks, setTasks ] = useState([
+    { name: 'Pagar Boleto', completed: false },
+    { name: 'Lavar o carro', completed: false },
+    { name: 'Fazer trabalho', completed: false },
+  ]);
+
+  const handleSubmit = (newTask) => {
+    setTasks([...tasks, newTask]);
+  }
+
+  const remove = (taskName) => {
+    const taskUpdate = tasks.filter(task => task.name !== taskName)
+    setTasks(taskUpdate)
+  }
+
+  const completed = (taskIndex) => {
+    const taskUpdate = [...tasks];
+    tasks[taskIndex].completed = !tasks[taskIndex].completed;
+    setTasks(taskUpdate);
+  }
+
+  return (
+  <div>
+    <span>Tasks concluídas</span>
+    <Form onSubmit={handleSubmit}/>
+    <ul>
+      {tasks.map((task, index) => <li key={task.name}>
+        {task.name}
+        <button onClick={() => remove(task.name)}>remove</button>
+        <button onClick={() => completed(index)}>
+          { task.completed ? "desmarcar" : "completar" }
+        </button>
+      </li>)}
+    </ul>
+  </div>
+  )
 }
 
 export default App
---
+++ src/components/Forms/index.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+export const Form = ({ onSubmit }) => {
+  const [ nameTask, setNameTask] = useState('');
+
+  const handleWriteTask = (event) => {
+    setNameTask(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newTask = { name: nameTask, completed: false }
+    onSubmit(newTask);
+    setNameTask('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+        <input type="text" value={ nameTask } onChange={ handleWriteTask }/>
+        <button type="submit">Criar Task</button>
+    </form>
+  )
+
+}
